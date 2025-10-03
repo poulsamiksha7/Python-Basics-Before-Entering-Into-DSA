30eaaa55d4255fd58dda9fcb4fc36ef4929d4824
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2025-10-04 00:09:30 +0530

    Add files via upload

---
+++ 40 Days Python Practice/Day5/question1.py
@@ -0,0 +1,5 @@
+# - List of fruits, update items
+list1=['apple','banana','citrus']
+print(list1)
+list1[0]='grapes'
+print(list1)
---
+++ 40 Days Python Practice/Day5/question2.py
@@ -0,0 +1,7 @@
+# - Append new item, print length
+list1=['anushka','sara','laila','kalpana']
+print(list1)
+print(len(list1))
+list1.append('anupama')
+print(list1)
+print(len(list1))
