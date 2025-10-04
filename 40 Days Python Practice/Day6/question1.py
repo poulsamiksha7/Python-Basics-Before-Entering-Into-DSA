ed018ebad79f05e5b76d5e73cf7183e227047aeb
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2025-10-04 22:57:32 +0530

    Add files via upload

---
+++ 40 Days Python Practice/Day6/question1.py
@@ -0,0 +1,6 @@
+# - Even numbers from list
+
+l1={2,4,5,6,78,9,7,5,80}
+for i in l1:
+    if i%2==0:
+        print(i)
---
+++ 40 Days Python Practice/Day6/question2.py
@@ -0,0 +1,9 @@
+# - List sum, max (manual)
+add=0
+l1={2,4,5,6,78,9,7,5,80}
+for i in l1:
+    add+=i
+    print(add)
+max_l1=max(l1)
+
+print(max_l1)
