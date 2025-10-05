78ae92003081c91b7694e214be6aab3e596709dc
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2025-10-05 21:59:25 +0530

    Add files via upload

---
+++ Patterns/rectangular_star_pattern.py
@@ -0,0 +1,21 @@
+n=int(input("Enter number: "))
+for i in range(0,n):
+    for j in range(0,n):
+        print("*",end="")
+    print()
+
+#Outer loop i=0
+# j=0,1,2,3,4  
+# print *****
+# Outer Loop i=1
+# j=1,2,3,4
+# print *****
+#Outer loop i=2
+# j=0,1,2,3,4
+# print *****
+#Outer loop i=3
+# j=0,1,2,3,4
+# print *****
+#Outer loop i=4
+# j=0,1,2,3,4
+# print *****
