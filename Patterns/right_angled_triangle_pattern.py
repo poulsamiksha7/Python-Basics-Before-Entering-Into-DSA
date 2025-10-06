32dddb041319f5fb14ba44d8c17c6554040d98c4
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2025-10-06 18:56:05 +0530

    Add files via upload

---
+++ Patterns/right_angled_triangle_pattern.py
@@ -0,0 +1,26 @@
+n=int(input("Enter Number: "))
+for i in range(0,n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+
+
+#Outer loop i=0
+#j=0
+#print= *
+
+#Outer loop i=1
+#j=0,1
+#print= **
+
+#Outer loop i=2
+#j=0,1,2
+#print= ***
+
+#Outer loop i=3
+#j=0,1,2,3
+#print= ****
+
+#Outer loop i=4
+#j=0,1,2,3,4
+#print= ******
